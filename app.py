3cba23a5eaba272a434a1dc59a3932399fc8bcc1
Laudemir <nlaudemir@gmail.com>
2021-12-13 14:52:48 -0300

    Atividade terminada

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-# Código do dev aqui
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return {'data': 'Hello Flask!'}
+
+@app.route('/current_datetime')
+def current_datetime():
+    data = datetime.now().strftime('%d/%m/%Y %H:%M:%S PM')
+    base = datetime.now().strftime('%H')
+    message = ""
+    if base < "12":
+        message = "Bom dia!"
+    elif base < "18":
+        message = "Boa tarde!"
+    else:
+        message = "Boa noite!"
+    return dict(current_datetime=data, message=message)
